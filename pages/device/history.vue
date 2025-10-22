ea0ff72e2c58d3968c018539f94ada6cb8d22099
Jason <jason@zgwit.com>
2025-10-23 01:45:19 +0800

    历史曲线能够正常显示

--- pages/device/history.vue
+++ pages/device/history.vue
@@ -1,18 +1,41 @@
 <template>
-	<view>
-		echarts
+	<view style="width:750rpx; height:750rpx">
+		<l-echart ref="chart" @finished="init"></l-echart>
 	</view>
 </template>
 
 <script>
+	import * as echarts from 'echarts'
 	export default {
 		data() {
 			return {
-				
-			}
+				option: {
+					legend: {
+						data: ['温度']
+					},
+					xAxis: {
+						type: 'category',
+						data: ['1时', '2时', '3时', '4时', '5时', '6时', '7时']
+					},
+					yAxis: {
+						type: 'value'
+					},
+					series: [{
+						name: '温度',
+						type: 'line',
+						smooth: true,
+						data: [30, 27, 34, 34.4, 30, 32, 31],
+					}]
+				},
+			};
 		},
+		// 组件能被调用必须是组件的节点已经被渲染到页面上
 		methods: {
-			
+			async init() {
+				// chart 图表实例不能存在data里
+				const chart = await this.$refs.chart.init(echarts);
+				chart.setOption(this.option)
+			}
 		}
 	}
 </script>
