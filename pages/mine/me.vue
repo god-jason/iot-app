adfc35a95a5c670b6da26f8c390281fe92245934
Jason <jason@zgwit.com>
2025-10-25 22:59:32 +0800

    改

--- pages/mine/me.vue
+++ pages/mine/me.vue
@@ -25,14 +25,6 @@
 			</uni-list>
 		</uni-card>
 
-		<uni-card title="平台管理" extra="限管理员">
-			<uni-list :border="false">
-				<uni-list-item title="所有设备" clickable show-arrow link="navigateTo" to="/pages/admin/device"></uni-list-item>
-				<uni-list-item title="所有组织" clickable show-arrow link="navigateTo" to="/pages/admin/group"></uni-list-item>
-				<uni-list-item title="所有用户" clickable show-arrow link="navigateTo" to="/pages/admin/user"></uni-list-item>
-			</uni-list>
-		</uni-card>
-
 
 	</view>
 </template>
