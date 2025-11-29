83f0c46f1d9482721c31496c48f751d2a6d2a546
jason <jason@zgwit.com>
2025-11-30 02:05:09 +0800

    封装MQTT

--- utils/broker.js
+++ utils/broker.js
@@ -6,6 +6,37 @@ import {
 
 var client
 
+
+let subs = {} // 订阅历史
+let sub_tree = {
+	children: {}, // topic->sub_tree
+	callbacks: []
+}
+
+// 查询订阅树
+function find_callback(node, topics, topic, payload) {
+	// 叶子节点，执行回调
+	if (topics.length == 0) {
+		node.callbacks.forEach(cb => cb(topic, payload))
+		return
+	}
+
+	let sub = node.children["#"]
+	sub && find_callback(sub, [], topic, payload)
+
+	let t = topics[0]
+	topics.splice(0, 1)
+
+	sub = node.children["+"]
+	sub && find_callback(sub, topics, topic, payload)
+
+	sub = node.children[t]
+	sub && find_callback(sub, topics, topic, payload)
+}
+
+
+
+
 export function connectMqtt() {
 
 	// #ifdef MP-WEIXIN
@@ -23,16 +54,81 @@ export function connectMqtt() {
 	client.on("error", console.error)
 	client.on("message", function(topic, payload) {
 		console.log("mqtt message", topic, payload.toString())
+		let ts = topic.split("/")
+		find_callback(sub_tree, ts, topic, payload.toString())
 	})
 	client.on("connect", function() {
 		console.log("mqtt connected")
+		// 恢复订阅
+		Object.keys(subs).forEach(s => {
+			if (subs[s] > 0)
+				client.subscribe(s)
+		})
 	})
 }
 
+
+
 export function subscribe(filter, cb) {
-	client.subscribe(filter)
+	// 计数，避免重复订阅
+	if (subs[filter] && subs[filter] > 0) {
+		subs[filter] = subs[filter] + 1
+	} else {
+		client && client.subscribe(filter)
+		subs[filter] = 1
+	}
+
+	let fs = filter.split("/")
+
+	// 创建树枝
+	let sub = self.sub_tree
+
+	fs.forEach(f => {
+		let s = sub.children[f]
+		if (!s) {
+			s = {
+				children: {},
+				callbacks: []
+			}
+			sub.children[f] = s
+		}
+		sub = s
+	})
+	sub.callbacks.push(cb)
 }
 
 export function unsubscribe(filter, cb) {
-	client.unsubscribe(filter)
+	// 取消订阅
+	if (subs[filter]) {
+		if (cb) {
+			subs[filter] = subs[filter] - 1
+			if (subs[filter] <= 0)
+				client.unsubscribe(filter)
+		} else {
+			client.unsubscribe(filter)
+		}
+	}
+
+	let fs = filter.split("/")
+
+	// 查树枝
+	let sub = sub_tree
+	fs.forEach(f => {
+		let s = sub.children[f]
+		if (!s)
+			return // 找不到了
+		sub = s
+	})
+
+	// 删除回调
+	if (callbacks.length > 1 && cb) {
+		for (var i = 0; i < sub.callbacks.length; i++) {
+			if (sub.callbacks[i] == cb) {
+				sub.callbacks.splice(i, 1)
+				break
+			}
+		}
+	} else {
+		sub.callbacks = {}
+	}
 }
