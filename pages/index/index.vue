a8e5f91a1ca9bab1c3e63593b1df6bf1d9867c72
JasonPeking <freewind6@protonmail.com>
2026-01-29 17:43:46 +0800

    1

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -3,7 +3,7 @@
 		<uni-card title="平台管理" extra="限管理员" v-if="user && user.admin">
 			<uni-list :border="false">
 				<uni-list-item title="所有设备" clickable show-arrow link="navigateTo" to="/pages/admin/device"></uni-list-item>
-				<uni-list-item title="所有组织" clickable show-arrow link="navigateTo" to="/pages/admin/group"></uni-list-item>
+				<uni-list-item title="所有组织" clickable show-arrow link="navigateTo" to="/pages/group/group"></uni-list-item>
 				<uni-list-item title="所有用户" clickable show-arrow link="navigateTo" to="/pages/admin/user"></uni-list-item>
 				<uni-list-item title="扫码设备" clickable @click="scanDevice"></uni-list-item>
 			</uni-list>
@@ -11,8 +11,8 @@
 
 		<uni-card title="组织管理" extra="我的组织" v-if="group">
 			<uni-list :border="false">
-				<uni-list-item title="组织详情" clickable show-arrow link="navigateTo" to="/pages/admin/group_detail"></uni-list-item>
-				<uni-list-item title="成员管理" clickable show-arrow link="navigateTo" to="/pages/index/members"></uni-list-item>
+				<uni-list-item title="组织详情" clickable show-arrow link="navigateTo" to="/pages/group/detail"></uni-list-item>
+				<uni-list-item title="成员管理" clickable show-arrow link="navigateTo" to="/pages/group/members"></uni-list-item>
 			</uni-list>
 		</uni-card>
 		
